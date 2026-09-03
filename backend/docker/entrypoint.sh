9c60182fcec092513d3918f36098c49cec3bf652
Artyr <Artyr@gamil.com>
2026-09-03 20:15:10 +0300

    I updated entrypoint.sh

--- backend/docker/entrypoint.sh
+++ backend/docker/entrypoint.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 set -e
 if [ -d "/etc/secrets" ]; then
-    chmod 600 /etc/secrets/*.pem 2>/dev/null || true
-    chown -R www-data:www-data /etc/secrets 2>/dev/null || true
+    chmod 644 /etc/secrets/*.pem 2>/dev/null || true
 fi
 php bin/console doctrine:migrations:migrate --no-interaction
 
