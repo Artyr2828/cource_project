148523f656dcbdc9714f8c69f9b3bf654e061391
Artyr <Artyr@gamil.com>
2026-09-05 20:33:37 +0300

    I made a proper project deployment

--- backend/docker/entrypoint.sh
+++ backend/docker/entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 set -e
-if [ -d "/etc/secrets" ]; then
-    chmod 644 /etc/secrets/*.pem 2>/dev/null || true
-fi
-php bin/console doctrine:migrations:migrate --no-interaction
 
+php bin/console doctrine:migrations:migrate --no-interaction
+nginx
 exec "$@"
