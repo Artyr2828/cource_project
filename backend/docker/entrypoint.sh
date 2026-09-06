f3003dedd41cdd5db3366915d5b7c8b5b2a49135
Artyr <Artyr@gamil.com>
2026-09-06 12:42:17 +0300

    Adding debug to the deployment

--- backend/docker/entrypoint.sh
+++ backend/docker/entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -e
-
+ls -la /etc/secrets || true
+ls -la /etc/secrets/private.pem || true
+ls -la /etc/secrets/public.pem || true
 php bin/console doctrine:migrations:migrate --no-interaction
 nginx
 exec "$@"
