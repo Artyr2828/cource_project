386e0508c94edba7cebfc025db5df1adf60ee1a8
Artyr <Artyr@gamil.com>
2026-09-06 18:28:24 +0300

    Add baseUrl in frontend

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,7 +4,7 @@ import router from './router'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://127.0.0.1:8000';
+axios.defaults.baseURL = 'https://cource-project-uphw.onrender.com';
 
 const app = createApp(App)
 
