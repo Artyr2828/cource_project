592854789c4d59edcd8ed101536d0b8b95fe9c37
Artyr <Artyr@gamil.com>
2026-09-06 19:34:58 +0300

    Add baseUrl in frontend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'https://cource-project-uphw.onrender.com/',
+  baseURL: 'https://cource-project-uphw.onrender.com',
   withCredentials: true
 });
 
